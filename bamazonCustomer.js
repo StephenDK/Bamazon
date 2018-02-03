e060e52cfc5e9ff006924d9a546ceee2bd7ed154
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-02-02 23:40:44 -0800

    Added database connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,6 +10,26 @@ var mysql = require('mysql');
 // Section 2
 // Functions and listeners
 
+// This functions connects to the database
+function connectToDatabase () {
+
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "StephenDKlein360",
+        database: "bamazon"
+    });
+
+    connection.connect(function(err) {
+        if (err) throw err;
+            console.log("Connected with id: " + connection.threadId);
+            connection.end();
+        
+    });
+}
+connectToDatabase();
+
 
 // Section 3
 // Main Process
