ea38729c15aab9ec2e156ec24a06013ff6433ce3
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-02-02 22:35:18 -0800

    Required mysql and inquirer modules

---
+++ bamazonCustomer.js
@@ -0,0 +1,15 @@
+// Section 1 
+// Global Variables
+
+// load the inquirer npm modules
+var inquirer = require('inquirer');
+
+// Load the mysql npm mdules
+var mysql = require('mysql');
+
+// Section 2
+// Functions and listeners
+
+
+// Section 3
+// Main Process
