fbd97ed1a528473e67bc1a00b37859c2c0394115
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-02-05 19:56:00 -0800

    Testing and debugging

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,25 +9,47 @@ var mysql = require('mysql');
 
 // Section 2
 // Functions and listeners
+// create connection to the database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "StephenDKlein360",
+    database: "bamazon"
+});
 
-
-
-
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "StephenDKlein360",
-        database: "bamazon"
-    });
-
-    connection.connect(function(err) {
-        if (err) throw err;
-            console.log("Connected with id: " + connection.threadId);
-            // connection.end();
+connection.connect(function(err) {
+    if (err) throw err;
+        console.log("Connected with id: " + connection.threadId + "\n");
+        console.log("Welcome to Bamazon!!");
         retrieveDatabaseData();
-    });
+            
+});
 
+// This function will prompt the user for item id and quantity wanted for purchase
+function startSalePrompt () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "productID",
+            message: "Enter the product id of which you would like to buy."
+        },
+        {
+            type: "input",
+            name: "quantityWanted",
+            message: "How many would you like to purchase?"
+        }
+    ]).then(function(purchaseAnswer) {
+        console.log(purchaseAnswer.productID);
+        console.log(purchaseAnswer.quantityWanted);
+        console.log("-----------");
+        var typeMe = purchaseAnswer.quantityWanted;
+
+        console.log(typeof typeMe);
+    })
+};
+
+// This function gets the data from the db and formats it with a loop
 function retrieveDatabaseData () {
     connection.query("SELECT * FROM products", function(err, res){
         if (err) throw err;
@@ -37,13 +59,12 @@ function retrieveDatabaseData () {
 
         var itemList = [];
         for (var i = 0; i < res.length; i++) {
-            itemList.push(res[i].product_name);
             
-            console.log(itemList[i]);
+            itemList.push(res[i]);
+            
+            console.log("Id:" + itemList[i].id + " " + res[i].product_name + " " + "$" + res[i].price);
         }
-        
-
-        connection.end();
+        startSalePrompt();
     })
 };
 
@@ -51,5 +72,6 @@ function retrieveDatabaseData () {
 
 // Section 3
 // Main Process
+
 
 
